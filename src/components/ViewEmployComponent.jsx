1265a3e0f1ebc8cce068c135a44d7bec26fac35b
Yulin <1710285545@qq.com>
2022-04-28 20:40:14 +0800

    完成员工视图

--- src/components/ViewEmployComponent.jsx
+++ src/components/ViewEmployComponent.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
 
 function ViewEmployComponent(props) {
+  const [employee, setEmployee] = useState({});
   const { id } = useParams();
 
+  useEffect(() => {
+    EmployeeService.getEmployeeById(id).then(({ data }) => {
+      setEmployee(data);
+    });
+  });
+
   return (
-    <div>
-      <h2>员工信息</h2>
+    <div className="card col-md-6 offset-md-3">
+      <h3 className="text-center">员工信息</h3>
+      <div className="card-body">
+        <div className="row">
+          <label>姓: {employee.firstName}</label>
+          <label>名: {employee.lastName}</label>
+          <label>电子邮件: {employee.emailId}</label>
+        </div>
+      </div>
     </div>
   );
 }
